958dcb177acca160be92d547790ced54a8fb1ac0
jayanti18 <jayantirajesh@yahoo.com>
2019-01-13 21:05:28 -0500

    updating the app

--- blueline-angular/README.md
+++ blueline-angular/README.md
@@ -1,4 +1,22 @@
 # BluelineAngular
+Blueline Bus Scheduler project displays the overall bus schedule of Blue line bus company for the current date.
+
+This is the front end (part 1) of the overall project where angular connect with mongodb collection Bus.
+When a new bus is added to the serivec the blueline employee can click New Bus tab from navigation bar and add new bus information to the table.
+Tab Bus Info from navbar lets the Blueline employee change/delete or view the bus records. 
+Roll based security to be add later.
+
+Part 2 of this project is blueline-bus-scheduler which is the backend of the project.
+
+To start this project  
+1. cd blueline-angular
+2. npm install
+3. ng serve
+
+Author: Jayanti Singh
+Tech Talent South, Charlotte NC
+Date: 01/11/2019
+
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.4.
 
--- blueline-angular/src/app/businfo/businfo.component.ts
+++ blueline-angular/src/app/businfo/businfo.component.ts
@@ -1,4 +1,7 @@
+import { Response } from '@angular/http';
+import { BusService } from '../services/bus.services';
 import { Component, OnInit } from '@angular/core';
+import Bus from '../models/bus.models';
 
 @Component({
   selector: 'app-businfo',
@@ -7,9 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusinfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    //Private busservice will be injected into the component by Angular Dependency Injector
+    private busService: BusService
+  ) { }
 
-  ngOnInit() {
+  //Declaring the new bus Object and initilizing it
+  public newBus: Bus = new Bus()
+
+  //An Empty list for the visible bus list
+  busesList: Bus[];
+  editBuses: Bus[] = [];
+
+
+  ngOnInit(): void {
+
+    //At component initialization the 
+    this.busService.getBuses()
+      .subscribe(buses => {
+        //assign the buslist property to the proper http response
+        this.busesList = buses
+        console.log(buses)
+      })
   }
-
+ 
+  editBus(bus: Bus) {
+    console.log(bus)
+     if(this.busesList.includes(bus)){
+      if(!this.editBuses.includes(bus)){
+        this.editBuses.push(bus)
+      }else{
+        this.editBuses.splice(this.editBuses.indexOf(bus), 1)
+        this.busService.editBus(bus).subscribe(res => {
+          console.log('Update Succesful')
+         }, err => {
+            this.editBus(bus)
+            console.error('Update Unsuccesful')
+          })
+        }
+      }
+    }
+    doneBus(bus:Bus){
+      bus.status = 'Done'
+      this.busService.editBus(bus).subscribe(res => {
+        console.log('Update Succesful')
+      }, err => {
+        this.editBus(bus)
+        console.error('Update Unsuccesful')
+      })
+    }
+    submitBus(event, bus:Bus){
+      if(event.keyCode ==13){
+        this.editBus(bus)
+      }
+    }
+    deleteBus(bus: Bus) {
+      this.busService.deleteBus(bus._id).subscribe(res => {
+        this.busesList.splice(this.busesList.indexOf(bus), 1);
+      })
+    }  
 }
